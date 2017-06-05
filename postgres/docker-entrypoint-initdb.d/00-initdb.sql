bedd7c962aa42481dc865096ccfb5c8024f4640e
Pedro Guilherme Rafael <pedrografael@gmail.com>
2017-06-05 00:44:12 +0000

    test table

--- postgres/docker-entrypoint-initdb.d/00-initdb.sql
+++ postgres/docker-entrypoint-initdb.d/00-initdb.sql
@@ -1,3 +1,14 @@
+CREATE SEQUENCE sq_test_id
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 99999999
+    CACHE 1;
+
 CREATE TABLE test(
-  id INTEGER NOT NULL
+  id INTEGER PRIMARY KEY DEFAULT nextval('sq_test_id'::regclass),
+  name character varying(32) NOT NULL,
+  created timestamp without time zone DEFAULT now() NOT NULL,
+  updated timestamp without time zone DEFAULT now()
 )
+
